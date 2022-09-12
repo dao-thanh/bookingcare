488d0159ce66c3e26d0e518c4e564f76a1c2d115
Benny-x21 <benny@enclave.vn>
2022-09-12 16:50:00 +0700

    get detail doctor information

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,3 +1,4 @@
+import res from "express/lib/response";
 import doctorService from "../services/doctorService"
 
 let getTopDoctorHome = async (req, res) => {
@@ -14,7 +15,45 @@ let getTopDoctorHome = async (req, res) => {
         })
     }
 }
+let getAllDoctors = async(req, res) => {
+    try{
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    }catch(e){
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
+let postInforDoctor = async (req, res) => {
+    try{
+        let response  = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response)
+    }catch(e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+let getDetailDoctorById = async(req, res) => {
+    try{
+        let infor = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(infor);
+    }catch(e) {
+        console.log(e);
+        res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
 
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInforDoctor: postInforDoctor,
+    getDetailDoctorById: getDetailDoctorById
 }
