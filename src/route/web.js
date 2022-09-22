d881b5dc82280fdbbe340b32183b81a8fe130407
Benny-x21 <benny@enclave.vn>
2022-09-22 10:31:02 +0700

    get all specialty

--- src/route/web.js
+++ src/route/web.js
@@ -2,7 +2,8 @@ import express from "express";
 import homeController from "../controllers/homeController"
 import userController from "../controllers/userController"
 import doctorController from "../controllers/doctorController"
-import patientContrller from "../controllers/patientController"
+import patientController from "../controllers/patientController"
+import specialtyController from "../controllers/specialtyController"
 
 let router = express.Router();
 
@@ -34,9 +35,12 @@ let initWebRoutes = (app) => {
     router.get('/api/get-extra-infor-doctor-by-id', doctorController.getExtraInforDoctorById);
     router.get('/api/get-profile-doctor-by-id', doctorController.getProfileDoctorById);
 
-    router.post('/api/patient-book-appointment', patientContrller.postBookAppointment)
-    router.post('/api/verify-book-appointment', patientContrller.postVerifyBookAppointment)
+    router.post('/api/patient-book-appointment', patientController.postBookAppointment)
+    router.post('/api/verify-book-appointment', patientController.postVerifyBookAppointment)
 
+    router.post('/api/create-new-specialty', specialtyController.createSpecialty)
+    router.get('/api/get-specialty', specialtyController.getAllSpecialty);
+    
     return app.use("/", router)
 }
 
