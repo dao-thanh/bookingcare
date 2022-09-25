1948b923845568f80e20dab652f7ad33f77aa9c5
Benny-x21 <benny@enclave.vn>
2022-09-25 11:22:27 +0700

    get clinic

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -11,6 +11,35 @@ let createClinic = async(req, res) => {
         })
     }
 }
+
+let getAllClinic = async(req, res) => {
+     try{
+        let infor = await clinicService.getAllClinic(req.body);
+        return res.status(200).json(infor);
+    }catch(e) {
+        console.log(e);
+        res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
+let getDetailClinicById = async(req, res) => {
+     try{
+        let infor = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(infor);
+    }catch(e) {
+        console.log(e);
+        res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+ 
 module.exports = {
-    createClinic: createClinic
+    createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
 }
